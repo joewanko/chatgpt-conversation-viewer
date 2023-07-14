1d6d5010513375912ae88e6135274b3a53c08aae
Joe Wanko <yo@joewanko.com>
2023-07-13 22:11:11 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import conversations from '../conversations.json';  // Path to your JSON file
+import React, { useEffect, useState, useRef } from 'react';
+import { Container, Form, Table } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import conversations from './conversations.json';  // Path to your JSON file
 
 function App() {
   const [formattedConversations, setFormattedConversations] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const searchRef = useRef(null);
 
   useEffect(() => {
     const formattedData = conversations.map(conversation => {
@@ -14,15 +17,31 @@ function App() {
         allMessages.push(nodeData.message.content.parts.join());
       });
 
+      const date = new Date(conversation.create_time * 1000);
       return {
         id: "https://chat.openai.com/c/" + conversation.id,
         title: conversation.title,
-        date: new Date(conversation.create_time * 1000).toISOString(),
-        firstMessage: allMessages[0].slice(0, 210),
+        date: date.toLocaleDateString(),
+        excerpt: allMessages[0].slice(0, 210),
         allMessages: allMessages.join(" "),
       };
     });
     setFormattedConversations(formattedData);
+  }, []);
+
+  useEffect(() => {
+    const keydownHandler = (event) => {
+      if ((event.metaKey || event.ctrlKey) && event.key === 'f') {
+        event.preventDefault();
+        if (searchRef.current) {
+          searchRef.current.focus();
+        }
+      }
+    };
+    window.addEventListener('keydown', keydownHandler);
+    return () => {
+      window.removeEventListener('keydown', keydownHandler);
+    };
   }, []);
 
   const handleChange = event => {
@@ -30,31 +49,55 @@ function App() {
   };
 
   const filteredConversations = formattedConversations.filter(conversation =>
-    conversation.allMessages.toLowerCase().includes(searchTerm.toLowerCase())
+    conversation.allMessages.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    conversation.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  // Function to highlight the search term
+  const highlightTerm = (text, term) => {
+    if (term !== "") {
+      const index = text.toLowerCase().indexOf(term.toLowerCase());
+      if (index >= 0) {
+        text = text.slice(0, index) + '<mark>' + text.slice(index, index + term.length) + '</mark>' + text.slice(index + term.length);
+      }
+    }
+    return { __html: text };
+  }
+
   return (
-    <div className="App">
-      <input type="text" placeholder="Search..." onChange={handleChange} />
-      <table>
+    <Container className="App">
+      <Form.Group className="py-3" style={{ position: 'sticky', top: 0, zIndex: 1, backgroundColor: 'white' }}>
+        <Form.Control ref={searchRef} type="text" placeholder="Search..." onChange={handleChange} />
+      </Form.Group>
+      <Table striped bordered hover>
         <thead>
           <tr>
             <th>Date</th>
             <th>Title</th>
-            <th>First Message</th>
+            <th>Excerpt</th>
           </tr>
         </thead>
         <tbody>
-          {filteredConversations.map((item, index) => (
-            <tr key={index}>
-              <td>{item.date}</td>
-              <td><a href={item.id} target="_blank" rel="noopener noreferrer">{item.title}</a></td>
-              <td>{item.firstMessage}</td>
-            </tr>
-          ))}
+          {filteredConversations.map((item, index) => {
+            const excerptIndex = item.allMessages.toLowerCase().indexOf(searchTerm.toLowerCase());
+            const start = searchTerm !== "" && excerptIndex >= 0
+              ? Math.max(0, excerptIndex - Math.floor((210 - searchTerm.length) / 2))
+              : 0;
+            const end = start + 210;
+            let excerpt = item.allMessages.slice(start, end);
+            if (start > 0) excerpt = '...' + excerpt;
+            if (end < item.allMessages.length) excerpt = excerpt + '...';
+            return (
+              <tr key={index}>
+                <td>{item.date}</td>
+                <td><a href={item.id} target="_blank" rel="noopener noreferrer">{item.title}</a></td>
+                <td dangerouslySetInnerHTML={highlightTerm(excerpt, searchTerm)} />
+              </tr>
+            );
+          })}
         </tbody>
-      </table>
-    </div>
+      </Table>
+    </Container>
   );
 }
 
